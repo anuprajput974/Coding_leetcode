6ce64c6d65b0dadf12b2af513580eca1afd98bd8
anuprajput974 <157352618+anuprajput974@users.noreply.github.com>
2025-07-16 20:49:00 +0530

    Time: 0 ms (100%), Space: 7.8 MB (96.64%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -3,7 +3,7 @@ public:
     int uniquePaths(int m, int n) {
         long long res = 1;
         int N = m + n - 2;
-        int k = min(m - 1, n - 1);  // We choose the smaller to reduce computation
+        int k = min(m - 1, n - 1);  
 
         for (int i = 1; i <= k; ++i) {
             res = res * (N - k + i) / i;
