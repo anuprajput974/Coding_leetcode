966b20ac500b438b2414b7888fb5ebfa6bc96d77
anuprajput974 <157352618+anuprajput974@users.noreply.github.com>
2025-07-17 16:54:40 +0530

    Time: 0 ms (100%), Space: 9.8 MB (26.89%) - LeetHub

--- 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.cpp
+++ 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.cpp
@@ -1,39 +1,57 @@
-#include <stack>
-
 class MyQueue {
-private:
-    std::stack<int> s1, s2;
-    
 public:
-    MyQueue() {}
-
+    
+    stack<int> input;
+    stack<int> output;
+    MyQueue() {
+       
+        
+    }
+    
     void push(int x) {
-        s1.push(x);
+        input.push(x);
+        
     }
-
+    
     int pop() {
-        if (s2.empty()) {
-            while (!s1.empty()) {
-                s2.push(s1.top());
-                s1.pop();
-            }
-        }
-        int front = s2.top();
-        s2.pop();
-        return front;
+        peek();
+        int val=output.top();
+        output.pop();
+        return val;
+        
     }
-
+    
     int peek() {
-        if (s2.empty()) {
-            while (!s1.empty()) {
-                s2.push(s1.top());
-                s1.pop();
+        if(output.empty()){
+            while(!input.empty()){
+                output.push(input.top());
+                input.pop();
             }
+           
         }
-        return s2.top();
+         return output.top();
     }
-
+    
     bool empty() {
-        return s1.empty() && s2.empty();
+        if(input.empty() && output.empty())
+        {
+          return true;
+        }
+          else{
+            return false;
+            }
+            
+        
+        
+        
     }
 };
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * MyQueue* obj = new MyQueue();
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * int param_3 = obj->peek();
+ * bool param_4 = obj->empty();
+ */
