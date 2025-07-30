8442e6ca96bff0a4c456994c78f00057d19698d4
anuprajput974 <157352618+anuprajput974@users.noreply.github.com>
2025-07-30 14:06:32 +0530

    Time: 0 ms (100%), Space: 8.2 MB (95.15%) - LeetHub

--- 2556-convert-the-temperature/2556-convert-the-temperature.cpp
+++ 2556-convert-the-temperature/2556-convert-the-temperature.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     vector<double> convertTemperature(double celsius) {
-        double arr[]={};
+       
         double kelvin=celsius+273.15;
         double fahrenheit=celsius*1.80+32.00;
         return {kelvin,fahrenheit};
