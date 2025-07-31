804a05e6d010f2484045dfa3538aac60e5611a21
anuprajput974 <157352618+anuprajput974@users.noreply.github.com>
2025-07-31 11:45:58 +0530

    Time: 47 ms (70.55%), Space: 29.1 MB (72.62%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -22,18 +22,25 @@ public:
              if(sum==0){
                 res.push_back({nums[i],nums[left],nums[right]});
 
-                while( left<right && nums[left]==nums[left+1]) left++;
-                while( left<right && nums[right]==nums[right-1]) right--;
+                while( left<right && nums[left]==nums[left+1]) 
+                {
+                    left++;
+                    };
+                while( left<right && nums[right]==nums[right-1]){ 
+                    right--;
+                    };
 
                 left++;
                 right--;
 
 
               }
-              else if(sum<0)
+              else if(sum<0){
                    left++;
-                   else
+              }
+                   else{
                    right--;
+                   }
 
            }
 
