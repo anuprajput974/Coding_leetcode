b7a2f917c38624ba7c16092bc748b17c0c26e5ad
anuprajput974 <157352618+anuprajput974@users.noreply.github.com>
2025-08-20 11:36:25 +0530

    Time: 76 ms (48.6%), Space: 174.4 MB (14.11%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -1,27 +1,33 @@
 class Solution {
 public:
+    string checkstring(string s,int left,int right){
+        while(left>=0 && right<s.length() && s[left]==s[right]){
+            left--;
+            right++;
+        }
+        return s.substr(left+1,right-left-1);
+    }
     string longestPalindrome(string s) {
-        if(s.empty()) return"";
-        int start=0,maxLength=1;
 
-        auto expandAroundCentre=[&](int left ,int right){
-            while(left>=0 && right<s.length() && s[left]==s[right]){
-                left--;
-                right++;
-            }
-            return right-left-1;
-        };
+        if(s.length()<=1) return s;
+        string longest="";
+        string curr;
+
         for(int i=0;i<s.length();i++){
-            int length1=expandAroundCentre(i,i);
-            int length2=expandAroundCentre(i,i+1);
-            int len=max(length1,length2);
-            if(len>maxLength){
-            maxLength=len;
-             start=i-(len-1)/2;
+
+           string odd=checkstring( s,i,i);
+           string even=checkstring( s,i,i+1);
+
+           if(odd.length()>even.length()){
+          curr=odd;
+           }else{
+             curr=even;
+           }
+
+           if(longest.length()<curr.length()){
+            longest=curr;
+           }
         }
-       
-        }
-    
-        return s.substr(start,maxLength);
+        return longest;
     }
 };
