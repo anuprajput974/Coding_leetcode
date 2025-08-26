fdd0a9841dcdec8eb09956508790b2333f078e65
anuprajput974 <157352618+anuprajput974@users.noreply.github.com>
2025-08-26 22:33:39 +0530

    Time: 945 ms (11.08%), Space: 34.4 MB (13.38%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.cpp
+++ 0127-word-ladder/0127-word-ladder.cpp
@@ -1,38 +1,32 @@
 class Solution {
 public:
     int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
-        unordered_set<string> wordSet(wordList.begin(), wordList.end());
-        
-        // If endWord is not in the dictionary, return 0
-        if (wordSet.find(endWord) == wordSet.end())
-            return 0;
-
-        queue<pair<string, int>> q;
-        q.push({beginWord, 1}); // Start with the beginWord and depth 1
-
-        while (!q.empty()) {
-            string word = q.front().first;
-            int length = q.front().second;
-            q.pop();
-
-            // Try changing every letter of the word
-            for (int i = 0; i < word.length(); ++i) {
-                string temp = word;
-                for (char c = 'a'; c <= 'z'; ++c) {
-                    temp[i] = c;
-                    // If we find the endWord
-                    if (temp == endWord)
-                        return length + 1;
-
-                    // If this transformed word is in the word set
-                    if (wordSet.find(temp) != wordSet.end()) {
-                        q.push({temp, length + 1});
-                        wordSet.erase(temp); // Mark word as visited
-                    }
+        unordered_set<string> dict(wordList.begin(), wordList.end());
+        queue<string> todo;
+        todo.push(beginWord);
+        int ladder = 1;
+        while (!todo.empty()) {
+            int n = todo.size();
+            for (int i = 0; i < n; i++) {
+string word = todo.front();
+                todo.pop();
+                if (word == endWord) {
+                    return ladder;
+                }
+                dict.erase(word);
+                for (int j = 0; j < word.size(); j++) {
+                    char c = word[j];
+                    for (int k = 0; k < 26; k++) {
+                        word[j] = 'a' + k;
+                        if (dict.find(word) != dict.end()) {
+                            todo.push(word);
+                        }
+                     }
+                    word[j] = c;
                 }
             }
+            ladder++;
         }
-
-        return 0; // No transformation found
+        return 0;
     }
 };
